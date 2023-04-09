b0e4167f73a3d5495e89e15518b03ae9c12499b5
Woody Lin <wancheng.lin001@umb.edu>
2023-04-09 14:53:01 -0400

    Update meme.js

--- js/meme.js
+++ js/meme.js
@@ -3,7 +3,7 @@
 function generateRandomImage() {
     const imageContainer = document.getElementById('image-container');
     const randomIndex = Math.floor(Math.random() * 2) + 1;
-    const imageUrl = 'img/redpanda/red' + randomIndex + '-min.jpg';
+    const imageUrl = '{{ site.baseurl }}/img/redpanda/red' + randomIndex + '-min.jpg';
     const randomImage = document.getElementById('random-image');
     randomImage.src = imageUrl;
   }
